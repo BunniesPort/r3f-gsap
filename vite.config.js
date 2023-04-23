e298949d6c4eb3a002fd9e62e851bcc357eeb964
BunniesPort <is0416ir@outlook.com>
2023-04-23 23:16:12 +0900

    :rocket: Add build files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES // この行を追加
+    ? "r3f-gsap" // この行を追加
+    : "./", // この行を追加
   plugins: [react()],
-})
+});
